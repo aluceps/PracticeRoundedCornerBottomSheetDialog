a8ff739ff3e719019c0bffc4a0ea3fe6a230faca
aluceps <aluceps@gmail.com>
2019-12-09 17:47:17 +0900

    implement empty bottom sheet dialog

--- app/src/main/java/me/aluceps/practicebottomsheetdialog/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicebottomsheetdialog/MainActivity.kt
@@ -1,9 +1,16 @@
 package me.aluceps.practicebottomsheetdialog
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import me.aluceps.practicebottomsheetdialog.databinding.ActivityMainBinding
+import me.aluceps.practicebottomsheetdialog.databinding.FragmentBottomSheetDialogBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +20,30 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding
+        binding.launch.setOnClickListener {
+            launchBottomSheetDialog()
+        }
+    }
+
+    private fun launchBottomSheetDialog() {
+        BottomSheetDialogFragment.newInstance()
+                .show(supportFragmentManager, "")
+    }
+}
+
+class BottomSheetDialogFragment : AppCompatDialogFragment() {
+
+    private lateinit var binding: FragmentBottomSheetDialogBinding
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+            BottomSheetDialog(context!!, theme)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentBottomSheetDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        fun newInstance() = BottomSheetDialogFragment()
     }
 }
