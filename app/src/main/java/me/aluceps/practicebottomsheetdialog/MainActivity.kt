02f3a4347913d126a4ee5d5e2471328003f0bda2
aluceps <aluceps@gmail.com>
2019-12-09 18:15:33 +0900

    add fragment

--- app/src/main/java/me/aluceps/practicebottomsheetdialog/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicebottomsheetdialog/MainActivity.kt
@@ -14,6 +14,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialog
 import me.aluceps.practicebottomsheetdialog.databinding.ActivityMainBinding
 import me.aluceps.practicebottomsheetdialog.databinding.FragmentBottomSheetDialogBinding
 import me.aluceps.practicebottomsheetdialog.databinding.FragmentBottomSheetDialogStep1Binding
+import me.aluceps.practicebottomsheetdialog.databinding.FragmentBottomSheetDialogStep2Binding
 
 class MainActivity : AppCompatActivity() {
 
@@ -58,11 +59,31 @@ class BottomSheetDialogStep1Fragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentBottomSheetDialogStep1Binding.inflate(inflater, container, false)
+        binding.next.setOnClickListener {
+            fragmentManager?.replaceFragment(BottomSheetDialogStep2Fragment.newInstance())
+        }
         return binding.root
     }
 
     companion object {
         fun newInstance() = BottomSheetDialogStep1Fragment()
+    }
+}
+
+class BottomSheetDialogStep2Fragment : Fragment() {
+
+    private lateinit var binding: FragmentBottomSheetDialogStep2Binding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentBottomSheetDialogStep2Binding.inflate(inflater, container, false)
+        binding.prev.setOnClickListener {
+            fragmentManager?.replaceFragment(BottomSheetDialogStep1Fragment.newInstance())
+        }
+        return binding.root
+    }
+
+    companion object {
+        fun newInstance() = BottomSheetDialogStep2Fragment()
     }
 }
 
